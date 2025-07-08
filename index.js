cb4cdd060125af256d35d4929fea2727fdc41178
Hamsikakrishnan <r.hamsikakrishnan@gmail.com>
2025-07-08 14:30:51 +0530

    Basic Rendering

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 import { menuArray } from './data.js'
-console.log(menuArray)
+
+
+const menu = document.getElementById("menu")
+
+
+function displayMenu(){
+    let html = ''
+    menuArray.forEach((menuItem) =>{
+        html += `
+        <div>
+            <h1>${menuItem.name}</h1>
+            <p>${menuItem.price}</p>
+            <p>${menuItem.emoji}</p>
+        </div>
+        `
+    })
+    return html
+}
+
+function render(){
+    menu.innerHTML = displayMenu()
+}
+
+render()
